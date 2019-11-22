a2e7b2d5ea9ed8ed190e21aac49a0eabb9c59494
unknown <jinyoung777@gmail.com>
2019-11-22 19:42:09 +1300

    create and edit - EmployeeController

---
+++ Paycompute/Models/EmployeeEditViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Models
+{
+    public class EmployeeEditViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee Number is required"), RegularExpression(@"^[A-Z]{3,3}[0-9]{3}$")]
+        public string EmployeeNo { get; set; }
+        [Required(ErrorMessage = "First Name is required"), StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z][a-zA-Z""'\s-]*$"), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [StringLength(50), Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required(ErrorMessage = "Last Name is required"), StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z][a-zA-Z""'\s-]*$"), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        [Display(Name = "Photo")]
+        public IFormFile ImageUrl { get; set; }
+        [DataType(DataType.Date), Display(Name = "Date Of Birth")]
+        public DateTime DOB { get; set; }
+        [DataType(DataType.Date), Display(Name = "Date Joined")]
+        public DateTime DateJoined { get; set; }
+        [Required(ErrorMessage = "Job Role is requird"), StringLength(100)]
+        public string Designation { get; set; }
+        public string Phone { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, StringLength(50), Display(Name = "Ni No.")]
+        [RegularExpression(@"^[A-Z]{1}[A-Z]{1}[0-9]{6}[A-D\s]$")]
+        public string NationalInsuranceNo { get; set; }
+        [Display(Name = "Payment Method")]
+        public PaymentMethod PaymentMethod { get; set; }
+        [Display(Name = "Student Loan")]
+        public StudentLoan StudentLoan { get; set; }
+        [Display(Name = "Union Member")]
+        public UnionMember UnionMember { get; set; }
+        [Required, StringLength(150)]
+        public string Address { get; set; }
+        [Required, StringLength(50)]
+        public string City { get; set; }
+        [Required, StringLength(50)]
+        public string Postcode { get; set; }
+    }
+}
